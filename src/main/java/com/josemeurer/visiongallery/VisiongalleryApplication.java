b2ad82f9aaf67a957e20daf1b6a25716f097b699
Jose Meurer <josewmeurer@gmail.com>
2026-03-08 16:43:59 -0300

    Configuração inicial

---
+++ src/main/java/com/josemeurer/visiongallery/VisiongalleryApplication.java
@@ -0,0 +1,13 @@
+package com.josemeurer.visiongallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisiongalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisiongalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josemeurer/visiongallery/VisiongalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemeurer.visiongallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisiongalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
